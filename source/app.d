4f126b3ca7ac745977728934c91b93a1c9e9f512
selimozel <sozel@wpi.edu>
2022-09-04 16:02:49 +0200

    Seems to have solved the update issue by cleaning the buffer

--- source/app.d
+++ source/app.d
@@ -46,7 +46,7 @@ void main(string[] args) {
   }
   http_call = "https://api.github.com/repos/"~
     user_inputs.github_name~"/"~user_inputs.github_repo~
-    "/commits";
+    "/commits/master";
   auto client = HTTP(http_call);
   client.addRequestHeader("Authorization", "token " ~ user_inputs.oauth_token);
   client.onReceive = (ubyte[] data) {
@@ -59,35 +59,34 @@ void main(string[] args) {
   while(1) {
     client.perform();
     github_response_json = parseJSON(github_response);
-    if(github_response_json.type == JSON_TYPE.ARRAY) {
-      new_commit = to!string(github_response_json[0]["sha"]);
-      new_commit_date = to!string(github_response_json[0]["commit"]["author"]["date"]);
-      if(new_commit != old_commit) {
-        auto pid_garbage = execute(["pidof", user_inputs.kill_list]);
-        if(pid_garbage.output!= null) {
-	        string pid_garbage_str = to!string(pid_garbage.output);
-	        pid_garbage_str = pid_garbage_str[0 .. pid_garbage_str.length - 1];
-	        auto pid_kill = execute(["kill", pid_garbage_str]);
-	        if (pid_kill.status != 0) writeln(pid_kill.output);
-	      }
-        if(old_commit == "") {
-          writeln("\033[1;33m[IntegrateD] Latest commit: "~
-            user_inputs.github_repo ~ " is "~ new_commit ~ " time stamp is " ~ new_commit_date);
-        } 
-        else {
-          writeln("\033[1;33m[IntegrateD] Old commit: "~
-            user_inputs.github_repo~ " is "~ old_commit);
-          writeln("\033[1;33m[IntegrateD] New commit: "~
-            user_inputs.github_repo~ " is "~ new_commit ~ " time stamp is " ~ new_commit_date);                 
-        }
-        old_commit = new_commit;
-        write("\033[1;32m\n");  
-        auto pid_ci = spawnShell(
-          user_inputs.ci_script,
-          null,
-          Config.none, 
-          user_inputs.ci_path);  
-      }      
-    } // github response
+    github_response = "";
+    new_commit = to!string(github_response_json["sha"]);
+    new_commit_date = to!string(github_response_json["commit"]["author"]["date"]);
+    if(new_commit != old_commit) {
+      auto pid_garbage = execute(["pidof", user_inputs.kill_list]);
+      if(pid_garbage.output!= null) {
+        string pid_garbage_str = to!string(pid_garbage.output);
+        pid_garbage_str = pid_garbage_str[0 .. pid_garbage_str.length - 1];
+        auto pid_kill = execute(["kill", pid_garbage_str]);
+        if (pid_kill.status != 0) writeln(pid_kill.output);
+      }
+      if(old_commit == "") {
+        writeln("\033[1;33m[IntegrateD] Latest commit: "~
+          user_inputs.github_repo ~ " is "~ new_commit ~ " time stamp is " ~ new_commit_date);
+      } 
+      else {
+        writeln("\033[1;33m[IntegrateD] Old commit: "~
+          user_inputs.github_repo~ " is "~ old_commit);
+        writeln("\033[1;33m[IntegrateD] New commit: "~
+          user_inputs.github_repo~ " is "~ new_commit ~ " time stamp is " ~ new_commit_date);                 
+      }
+      old_commit = new_commit;
+      write("\033[1;32m\n");  
+      auto pid_ci = spawnShell(
+        user_inputs.ci_script,
+        null,
+        Config.none, 
+        user_inputs.ci_path);  
+    } // commit update   
   } // ci loop
 } // main
